65ec91a8490b4466838c058f9c3399a840265737
Sreehari S <103099220+SreehariTheProgrammer@users.noreply.github.com>
2022-04-17 18:08:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hi! I am Program, your friendly Neighbourhood AI!")
+
+while True:
+    print("")
+    inp = input("Command: ")
+    print("")
+
+    if inp.lower() == "hi":
+        print("Hello Master")
+
+    elif inp.lower() == "hello":
+        print("Hi Master")
+
+    elif inp.lower() == "bye":
+        print("Bye Master")
